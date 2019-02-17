2db5389276f69a0294895044d6d638153fdfa07a
linrong <linrong123@qq.com>
2019-02-18 09:47:57 +1100

    Updates

---
+++ precache-manifest.80cfcd108463558e2536d1f3ede2c1a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a947ab33f7eaf0cf164e4b5829cc2e7",
+    "url": "/react-github/static/media/Spinner-1s-200px.2a947ab3.gif"
+  },
+  {
+    "revision": "bad057cd5df22fac7b39",
+    "url": "/react-github/static/js/runtime~main.bad057cd.js"
+  },
+  {
+    "revision": "e0c87f77826ad762e87a",
+    "url": "/react-github/static/js/main.e0c87f77.chunk.js"
+  },
+  {
+    "revision": "54c079bdaac82155bad0",
+    "url": "/react-github/static/js/5.54c079bd.chunk.js"
+  },
+  {
+    "revision": "868936c295c9720cb2dd",
+    "url": "/react-github/static/js/4.868936c2.chunk.js"
+  },
+  {
+    "revision": "de6876c3eb7699696f99",
+    "url": "/react-github/static/js/3.de6876c3.chunk.js"
+  },
+  {
+    "revision": "096cd738b608c5194d3f",
+    "url": "/react-github/static/js/2.096cd738.chunk.js"
+  },
+  {
+    "revision": "10acfe501c1323a2eff9",
+    "url": "/react-github/static/js/0.10acfe50.chunk.js"
+  },
+  {
+    "revision": "e0c87f77826ad762e87a",
+    "url": "/react-github/static/css/main.e2034542.chunk.css"
+  },
+  {
+    "revision": "7ed8c908fb7c215c6059bdac7e3311d8",
+    "url": "/react-github/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-github/precache-manifest.5f94434ec774375ce61d632b30083a53.js"
+  "/react-github/precache-manifest.80cfcd108463558e2536d1f3ede2c1a1.js"
 );
 
 workbox.clientsClaim();
